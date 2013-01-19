09112c122a2c941a98651d81cfb08e050879e195
Bill Gathen <bill@billgathen.com>
2013-01-18 19:35:58 -0500

    Single-method version complete

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,15 @@
-(1..15).each do |n|
-  if (n % 15) == 0
-    puts "FizzBuzz"
-  elsif (n % 5) == 0
-    puts "Buzz"
-  elsif (n % 3) == 0
-    puts "Fizz"
-  else
-    puts n
+def fizzbuzz up_to
+  (1..up_to).each do |n|
+    if (n % 15) == 0
+      puts "FizzBuzz"
+    elsif (n % 5) == 0
+      puts "Buzz"
+    elsif (n % 3) == 0
+      puts "Fizz"
+    else
+      puts n
+    end
   end
 end
+
+fizzbuzz(15)
